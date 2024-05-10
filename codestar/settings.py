505bbdf272a3b9a7470b2a6703cdad30e78378b4
Joannamcgallagher <joannamcgallagher@gmail.com>
2024-05-10 20:24:24 +0000

    Add env.py file back in & fix settings to run

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fhmm-j=n-lbp74j-340m^x-+p*ov-=5$7)7a*(1@_h%hcojzow'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-joannamcgall-djangoblog-oi976n4dlov.ws-eu111.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-joannamcgall-djangoblog-rvsi9asbn47.ws-eu111.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -89,7 +89,7 @@ DATABASES = {
     'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.gitpod.io",
