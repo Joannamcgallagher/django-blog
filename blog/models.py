df9f28dc340e3ff68402996e186ed1de0dadce26
Joannamcgallagher <joannamcgallagher@gmail.com>
2024-05-10 20:57:43 +0000

    Alter and debug the database

--- blog/models.py
+++ blog/models.py
@@ -17,9 +17,23 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
+    class Meta:
+        ordering = ["-created_on"] #the "-" befire will order the posts by newest first
+    #methods should always be under the Meta classes
+    def __str__(self):
+        return f"The tile of this post is {self.title} | written by {self.author}"
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     body = models.TextField()
     approved = models.BooleanField(default = False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    #Meta method to sort comments
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
